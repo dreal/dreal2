b13e6a950cd146d107059d058e065d78da85f110
Soonho Kong <soonhok@cs.cmu.edu>
2015-01-28 05:18:12 -0500

    fix(opensmt/egraph/Enode.C): use std::fix to avoid scientific notation problem in CAPD
    close #73

--- src/opensmt/egraph/Enode.C
+++ src/opensmt/egraph/Enode.C
@@ -343,7 +343,7 @@ void Enode::print_infix(ostream & os, lbool polarity, string const & variable_po
         if (name.find('e') != std::string::npos || name.find('E') != std::string::npos) {
             // Scientific Notation
             double r = *(symb_data->value);
-            os << setprecision(16) << r;
+            os << setprecision(16) << std::fixed << r;
         } else {
             // Fixed Notation
             os << name;
